10319ad9e146020e1b88fa6586d24eb767849f47
rq-kazushiwaki-at-321899771753 <7zcorgizyonna1144@gmail.com>
2024-12-01 12:11:15 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  has_many :recipes, dependent: :destroy
-=======
   has_many :recipes, dependent: :destroy  #user対recipe→1対多
->>>>>>> new-branch-name2
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
